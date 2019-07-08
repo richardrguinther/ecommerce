7283fded4250da199aaabf061fc915324af6e6ea
Richard <richard.rguinther@gmail.com>
2019-07-07 23:24:17 -0300

    Feito os templates e a classe Page.

---
+++ views-cache/header.e34bdcf785bf37c97bebe0d5c2cc8350.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Hcode Store</title>
+</head>
+
+<body>
