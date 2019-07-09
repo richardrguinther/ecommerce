f016cea859baeedf244812ab78976fe080c15fbf
Richard <richard.rguinther@gmail.com>
2019-07-09 17:29:59 -0300

    Tela de login/admin.

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,11 +9,15 @@ class Page
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header" => true,
+        "footer" => true,
         "data" => []
     ];
 
     public function __construct($opts = array(), $tpl_dir = "/views/")
     {
+        // $this->defaults["data"]["session"] = $_SESSION;
+
         $this->options = array_merge($this->defaults, $opts);
 
         $config = array(
@@ -28,25 +32,25 @@ class Page
 
         $this->setData($this->options["data"]);
 
-        $this->tpl->draw("header");
+        if ($this->options["header"] === true) $this->tpl->draw("header");
     }
 
     private function setData($data = array())
     {
-        foreach($data as $key => $value){
-            $this->tpl->assign($key,$value);
+        foreach ($data as $key => $value) {
+            $this->tpl->assign($key, $value);
         }
     }
 
     public function setTpl($name, $data = array(), $returnHTML = false)
-    { 
+    {
         $this->setData($data);
 
-        return $this->tpl->draw($name,$returnHTML);
+        return $this->tpl->draw($name, $returnHTML);
     }
 
     public function __destruct()
     {
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] === true) $this->tpl->draw("footer");
     }
 }
