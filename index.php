961cd5ed9d05c57b9125d3c59ab6a86b818948cb
Richard <richard.rguinther@gmail.com>
2019-07-15 18:00:30 -0300

    Feito o CRUD de categorias.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use Slim\Slim;
 use Hcode\Page;
 use Hcode\PageAdmin;
 use Hcode\Model\User;
+use Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -163,7 +164,7 @@ $app->post("/admin/forgot/reset", function () {
 
 	$user->get((int) $forgot["iduser"]);
 
-	$password = password_hash($_POST["password"], PASSWORD_DEFAULT,[
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
 		"cost" => 12
 	]);
 
@@ -175,6 +176,75 @@ $app->post("/admin/forgot/reset", function () {
 	]);
 
 	$page->setTpl("forgot-reset-success");
+});
+
+$app->get("/admin/categories", function () {
+	User::verifyLogin();
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", array(
+		"categories" => $categories
+	));
+});
+
+$app->get("/admin/categories/create", function () {
+	User::verifyLogin();
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function () {
+	User::verifyLogin();
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("location:/admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location:/admin/categories");
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", array(
+		"category" => $category->getValues()
+	));
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location:/admin/categories");
+	exit;
 });
 
 $app->run();
