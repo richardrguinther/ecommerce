da816bf0f91e917e021298f39945f8cf8c772d74
Richard <richard.rguinther@gmail.com>
2019-07-21 20:37:34 -0300

    Carrinho completo.

--- site.php
+++ site.php
@@ -63,7 +63,8 @@ $app->get("/cart", function () {
 
     $page->setTpl("cart", array(
         "cart" => $cart->getValues(),
-        "products" => $cart->getProducts()
+        "products" => $cart->getProducts(),
+        "error" => Cart::getMsgError()
     ));
 });
 
@@ -107,5 +108,14 @@ $app->get("/cart/:idproduct/remove", function ($idproduct) {
     $cart->removeProduct($product, true);
 
     header("Location:/cart");
+    exit;
+});
+
+$app->post("/cart/freight", function() {
+    $cart = Cart::getFromSession();
+
+    $cart->setFreight($_POST["zipcode"]);
+
+    header("Location: /cart");
     exit;
 });
